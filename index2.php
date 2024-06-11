2388fc8ead79dda7939de2d27ce2704aa102312b
ekasfitri <ekafitrisafitri7@gmail.com>
2024-06-11 15:08:06 +0700

    menambahkan file index2.php

---
+++ index2.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index2 git</title>
+</head>
+<body>
+   <h1>Eka Safitri : 22110004</h1> 
+</body>
+</html>
